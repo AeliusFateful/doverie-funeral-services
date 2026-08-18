08d97d1d7422f5522207cee2770a1582ea4d6ea3
Ilyz Zhukov <goukilya5@gmail.com>
2026-08-18 16:59:40 +0700

    Updates

--- _next/static/Tq1WhBn0MeoG3KQOA1wiw/_buildManifest.js
+++ _next/static/Tq1WhBn0MeoG3KQOA1wiw/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/doverie-funeral-services//_next/:path+",
-        "destination": "/doverie-funeral-services/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
